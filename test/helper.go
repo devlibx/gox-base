dbbef9d50f28f095b363e4ae6c3f0ddb975a5ba5
Harish Bohara <you@example.com>
2021-05-14 16:36:36 +0530

    added more generic cross function mock function

--- test/helper.go
+++ test/helper.go
@@ -5,9 +5,47 @@ import (
 	mockGox "github.com/devlibx/gox-base/mocks"
 	"github.com/golang/mock/gomock"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"go.uber.org/zap/zaptest"
 	"testing"
 )
+
+func MockCf(args ...interface{}) (gox.CrossFunction, *gomock.Controller) {
+	var controller *gomock.Controller
+	var logger *zap.Logger
+	logLevel := zap.ErrorLevel
+
+	// See if we have got a controller from client
+	for _, arg := range args {
+		switch o := arg.(type) {
+		case *gomock.Controller:
+			controller = o
+		case zapcore.Level:
+			logLevel = o
+		}
+	}
+
+	// build dummy cf
+	for _, arg := range args {
+		switch o := arg.(type) {
+		case *testing.T:
+			if controller == nil {
+				controller = gomock.NewController(o)
+			}
+			logger = zaptest.NewLogger(o, zaptest.Level(logLevel))
+		case *testing.B:
+			if controller == nil {
+				controller = gomock.NewController(o)
+			}
+			logger = zaptest.NewLogger(o, zaptest.Level(logLevel))
+		}
+	}
+
+	// Build cross function and return
+	cf := mockGox.NewMockCrossFunction(controller)
+	cf.EXPECT().Logger().Return(logger).AnyTimes()
+	return cf, controller
+}
 
 func BuildMockCf(t *testing.T, controller *gomock.Controller) gox.CrossFunction {
 	cf := mockGox.NewMockCrossFunction(controller)
--- test/helper_test.go
+++ test/helper_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"flag"
 	"github.com/golang/mock/gomock"
+	"go.uber.org/zap"
 	"testing"
 )
 
@@ -15,5 +16,10 @@ func init() {
 func TestBuildMockCf(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	cf := BuildMockCf(t, ctrl)
+	cf.Logger().Debug("make sure I do not crash.")
+}
+
+func TestMockCf(t *testing.T) {
+	cf, _ := MockCf(t, zap.DebugLevel)
 	cf.Logger().Debug("make sure I do not crash.")
 }
