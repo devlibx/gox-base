4a8e38076d8aac09c6640e34f92c282c55a0d019
Harish Bohara <you@example.com>
2021-07-10 05:41:50 +0530

    Added OpentracingLogError

--- util/trace_util.go
+++ util/trace_util.go
@@ -9,3 +9,33 @@ func OpentracingLogError(spanName string, err error) {
 		span.Finish()
 	}
 }
+
+func OpentracingLogError1(spanName string, err error, key string, value interface{}) {
+	if opentracing.GlobalTracer() != nil {
+		span := opentracing.GlobalTracer().StartSpan(spanName)
+		span.SetTag("error", err)
+		span.SetTag(key, value)
+		span.Finish()
+	}
+}
+
+func OpentracingLogError2(spanName string, err error, key string, value interface{}, key1 string, value1 interface{}) {
+	if opentracing.GlobalTracer() != nil {
+		span := opentracing.GlobalTracer().StartSpan(spanName)
+		span.SetTag("error", err)
+		span.SetTag(key, value)
+		span.SetTag(key1, value1)
+		span.Finish()
+	}
+}
+
+func OpentracingLogError3(spanName string, err error, key string, value interface{}, key1 string, value1 interface{}, key2 string, value2 interface{}) {
+	if opentracing.GlobalTracer() != nil {
+		span := opentracing.GlobalTracer().StartSpan(spanName)
+		span.SetTag("error", err)
+		span.SetTag(key, value)
+		span.SetTag(key1, value1)
+		span.SetTag(key2, value2)
+		span.Finish()
+	}
+}
