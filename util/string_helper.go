4c0d730cddff28871d913db27fe223ac6c7286be
Harish Bohara <harish.bohara@gmail.com>
2022-11-10 13:41:07 +0530

    added StringToHashMod

--- util/string_helper.go
+++ util/string_helper.go
@@ -1,7 +1,18 @@
 package util
 
-import "strings"
+import (
+	"hash/fnv"
+	"math"
+	"strings"
+)
 
 func IsStringEmpty(input string) bool {
 	return len(strings.TrimSpace(input)) <= 0
+}
+
+func StringToHashMod(item string, count int) int {
+	h := fnv.New64()
+	_, _ = h.Write([]byte(item))
+	sha := h.Sum64() % uint64(count)
+	return int(math.Abs(float64(sha)))
 }
