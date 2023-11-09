ecdbafa86d181e0f1802b8dd7725d928dc3b9081
Harish Bohara <harish.bohara@gmail.com>
2023-11-09 16:56:47 +0530

    added hook

--- go.mod
+++ go.mod
@@ -16,14 +16,12 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475
 	github.com/sethvargo/go-retry v0.2.4
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.8.0
 	github.com/urfave/negroni v1.0.0
-	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.16.0
+	go.uber.org/fx v1.20.1
+	go.uber.org/zap v1.23.0
 	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
 	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
 	gopkg.in/tylerb/graceful.v1 v1.2.15
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
-	honnef.co/go/tools v0.1.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1
 )
--- server/server.go
+++ server/server.go
@@ -1,8 +1,11 @@
 package goxServer
 
 import (
+	"context"
+	"fmt"
 	"github.com/devlibx/gox-base"
 	"github.com/devlibx/gox-base/config"
+	"go.uber.org/fx"
 	"net/http"
 	"sync"
 )
@@ -13,6 +16,7 @@ type Server interface {
 }
 
 type ServerShutdownHook interface {
+	Setup(interface{})
 	StopFunction() func()
 }
 
@@ -29,4 +33,45 @@ func NewServerWithShutdownHookFunc(cf gox.CrossFunction, shutdownHookFunc func()
 func NewServerWithShutdownHook(cf gox.CrossFunction, serverShutdownHook ServerShutdownHook) (Server, error) {
 	s := &serverImpl{CrossFunction: cf, shutdownHookFunc: serverShutdownHook.StopFunction(), stopOnce: &sync.Once{}}
 	return s, nil
+}
+
+// noOpServerShutdownHook - dummy shutdown hook
+type noOpServerShutdownHook struct {
+}
+
+func (h noOpServerShutdownHook) Setup(set interface{}) {
+}
+
+func (noOpServerShutdownHook) StopFunction() func() {
+	return func() {
+	}
+}
+
+func NoOpServerShutdownHook() ServerShutdownHook {
+	return &noOpServerShutdownHook{}
+}
+
+// FxServerShutdownHook is a common fx
+type FxServerShutdownHook struct {
+	FxApp *fx.App
+}
+
+func (h *FxServerShutdownHook) Setup(i interface{}) {
+	if a, ok := i.(*fx.App); ok {
+		h.FxApp = a
+	} else {
+		fmt.Println("*********** Something is wrong - we expected *fx.App in FxServerShutdownHook.Setup() *********** ")
+	}
+}
+
+func (h *FxServerShutdownHook) StopFunction() func() {
+	return func() {
+		if h.FxApp != nil {
+			_ = h.FxApp.Stop(context.Background())
+		}
+	}
+}
+
+func NewFxServerShutdownHook() ServerShutdownHook {
+	return &FxServerShutdownHook{}
 }
