4504cc092fb84ad45dee3b278239a14eaf2c0cce
Harish Bohara <harish.bohara@gmail.com>
2025-01-12 01:20:38 +0530

    added enc/dec utils

--- encdec/noop.go
+++ encdec/noop.go
@@ -1,19 +1,25 @@
 package encryption
 
+// NoOpServiceFactory is a factory to create encryptor decryptor service
+// This is a no-op implementation of the service factory
 type noOpServiceFactoryImpl struct {
 }
 
+// GetEncryptorDecryptService returns a new instance of No-OP EncryptorDecryptService
 func (n noOpServiceFactoryImpl) GetEncryptorDecryptService(groupName string) (EncryptorDecryptService, error) {
 	return &noOpEncryptorDecryptService{}, nil
 }
 
+// noOpEncryptorDecryptService a no-op implementation of EncryptorDecryptService
 type noOpEncryptorDecryptService struct {
 }
 
+// EncryptAndOutputBase64Ciphertext no-op, just return the input as output
 func (n noOpEncryptorDecryptService) EncryptAndOutputBase64Ciphertext(data string) (string, error) {
 	return data, nil
 }
 
+// DecryptFromBase64Ciphertext no-op, just return the input as output
 func (n noOpEncryptorDecryptService) DecryptFromBase64Ciphertext(data string) (string, error) {
 	return data, nil
 }
