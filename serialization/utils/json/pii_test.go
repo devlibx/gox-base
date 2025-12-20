427acb1e157bb7e54d41b16b4c75dce81a8668bb
Harish Bohara <harish.bohara@gmail.com>
2025-12-20 11:48:12 +0530

    fixed masking struct

--- serialization/utils/json/pii_test.go
+++ serialization/utils/json/pii_test.go
@@ -223,4 +223,39 @@ func TestPrettyStringLoggingSuppressError(t *testing.T) {
 
 		fmt.Println(result)
 	})
+
+	t.Run("masking with struct object pointer", func(t *testing.T) {
+		type requestObj struct {
+			Type   string `json:"type"`
+			UserId string `json:"user_id"`
+			MailId string `json:"mail_id"`
+		}
+
+		goxJsonUtils.RegisterPiiMasker("mail_id", func(mailId string) string {
+			return "masked-mail-id"
+		})
+		defer goxJsonUtils.ClearPiiMaskersForTesting()
+
+		input := &requestObj{
+			Type:   "test_request",
+			UserId: "user-123",
+			MailId: "test.user@example.com",
+		}
+
+		expected := `{
+	"type": "test_request",
+	"user_id": "user-123",
+	"mail_id": "masked-mail-id"
+}`
+
+		// Test with struct value
+		result := goxJsonUtils.PrettyStringLoggingSuppressError(input)
+		assert.JSONEq(t, expected, result)
+
+		// Test with pointer to struct
+		result = goxJsonUtils.PrettyStringLoggingSuppressError(&input)
+		assert.JSONEq(t, expected, result)
+
+		fmt.Println(result)
+	})
 }
